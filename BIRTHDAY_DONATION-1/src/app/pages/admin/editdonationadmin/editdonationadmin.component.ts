7ee486d185a6e32c29917db0f84fc049014bf5b9
jainam01 <89692451+jainam01@users.noreply.github.com>
2022-07-27 19:36:02 +0530

    todo thoduk karyu chhe but error aave chhe solve karva nu try karyu but nai thatu

--- BIRTHDAY_DONATION-1/src/app/pages/admin/editdonationadmin/editdonationadmin.component.ts
+++ BIRTHDAY_DONATION-1/src/app/pages/admin/editdonationadmin/editdonationadmin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CrudService } from 'src/app/service/crud.service';
+import { Task } from 'src/app/task';
 
 @Component({
   selector: 'app-editdonationadmin',
@@ -6,10 +8,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./editdonationadmin.component.css']
 })
 export class EditdonationadminComponent implements OnInit {
+  
+  taskObj : Task = new Task();
+  taskArr : Task[] = [];
 
-  constructor() { }
+  addTaskValue : string = '';
+
+  constructor(private crudService : CrudService) { }
 
   ngOnInit(): void {
+    this.taskObj = new Task();
+    this.taskArr = [];
+    this.getAllTask();
+  }
+  getAllTask(){
+    this.crudService.getAllTask().subscribe(res => {
+      this.taskArr = res;
+    }, err => {
+      alert("Unable to get list");
+    })
+  }
+
+  addTask(){
+    this.taskObj.task_name = this.addTaskValue;
+    this.crudService.addTask(this.taskObj).subscribe(res =>{
+      this.ngOnInit();
+      this.addTaskValue = '';
+    }, err => {
+      alert(err);
+    })
+  }
+
+  editTask(){
+    this.crudService.editTask(this.taskObj).subscribe( res => {
+      this.ngOnInit();
+    }, err=> {
+      alert("failed to delete task");
+    })
+  }
+
+  deleteTask(etask : Task){
+    this.crudService.deleteTask(etask).subscribe( res => {
+      this.ngOnInit();
+    }, err =>{
+      alert("faild to delete task");
+    });
   }
 
 }
